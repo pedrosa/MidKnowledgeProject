513b23b8c4191896b446b163ebc3361a50f896de
Pedro <pedro.sa@stone.com.br>
2023-02-09 09:34:29 -0300

    add domain project and person class

---
+++ MKP.Data/MainContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MKP.Domain;
+
+namespace MKP.Data;
+
+public class MainContext : DbContext
+{
+    public MainContext(DbContextOptions options) : base(options)
+    {
+    }
+    
+    public DbSet<Person> Person { get; set; }
+}
---
+++ MKP.Domain/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MKP.Domain;
+
+public class Person
+{
+    [Key]
+    public Guid Id { get; set; }
+    
+    [Required(ErrorMessage = "The field {0} is required.")]
+    [MaxLength(50, ErrorMessage = "The max lenght to the field {0} is {1}.")]
+    public string Name { get; set; }
+    
+    public Guid Document { get; set; }
+    
+    public int Age { get; set; }
+}
